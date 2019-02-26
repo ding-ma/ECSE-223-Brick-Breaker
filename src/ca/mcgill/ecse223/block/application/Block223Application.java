1ab5020e828a5af3ccc14d663aece230ed7b7e41
George Kandalaft <46607090+Georges1998@users.noreply.github.com>
2019-02-26 14:37:41 -0500

    Update Block223Application.java

--- src/ca/mcgill/ecse223/block/application/Block223Application.java
+++ src/ca/mcgill/ecse223/block/application/Block223Application.java
@@ -16,19 +16,31 @@ public class Block223Application {
             }
         });
 	}
-
+	//George
 	public static Block223 getBlock223() {
 		if (block223 == null) {
 			block223 = new Block223();
 		}
  		return block223;
 	}
-	
+	//George
 	public static Game getCurrentGame() {
 		if (game == null) {
 			game = new Game(null, 0, null, null, null, block223);
 		}
  		return game;
+		
+	}
+	//George
+	public static void setCurrentUserRole(UserRole aUserRole) {
+		userRole = aUserRole;
+	}
+	//George
+	public static UserRole getCurrentUserRole() {
+		if (userRole == null) {
+			return null;
+		}
+		return userRole;
 		
 	}
 	
