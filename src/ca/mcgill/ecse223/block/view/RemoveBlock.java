4e813af7c7fa4c3849bf9739e875403503157f79
annejuliec <anne-julie.cote@mail.mcgill.ca>
2019-03-04 20:17:29 -0500

    update and remove block UI

--- src/ca/mcgill/ecse223/block/view/RemoveBlock.java
+++ src/ca/mcgill/ecse223/block/view/RemoveBlock.java
@@ -1,59 +1,103 @@
 package ca.mcgill.ecse223.block.view;
 
+import java.awt.Color;
+
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import ca.mcgill.ecse223.block.controller.*;
 
+import ca.mcgill.ecse223.block.application.Block223Application;
+import ca.mcgill.ecse223.block.controller.Block223Controller;
 public class RemoveBlock {
-    private JButton Deletes = new JButton();
-    private JFrame Box = new JFrame();
-    private JTextField blockselected = new JTextField();
-    private JComboBox list = new JComboBox();
 
-    public void DeleteBlock (){
+	
+	private String error = null;
+	private JLabel errorMessage;
 
-            list.setBounds(100,100,200,50);
-            for (int k=0; k<3; k++){ //100 will be the number of blocks or something close to that
-                list.addItem("123");//this should be all the blocks
-            }
-            list.addItem("first");
-            list.addItem("second");
-            list.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e){
-                    list.getSelectedItem();
-                    blockselected.setText(""+list.getSelectedItem());
+	private JTextField levelField;
+	private JTextField oldGridHorizontalField;
+	private JTextField oldGridVerticalField;
 
-                    int id;
-                }
-            });
-            Box.add(list);
+	private JLabel moveBlockLabel;
 
-            Deletes.setBounds(20,30,200,50);
-            Deletes.setText("Remove Block");
-            //   Deletes.setFont(main.font);
-            Box.add(Deletes);
-            Deletes.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    System.out.println(list.getSelectedItem());
-                    Block223Controller block223Controller = new Block223Controller();
-                    block223Controller.getCurrentDesignableGame().delete();
+	private JLabel levelNumber;
+	private JLabel oldGridHorizontalValue;
+	private JLabel oldGridVerticalValue;
 
-       
-                }
-            });
-            blockselected.setEditable(false);
-            blockselected.setVisible(true);
-            blockselected.setBounds(200,200,200,50);
-            blockselected.setBackground(Color.white);
-            Box.add(blockselected);
+	private JButton removeButton;
+	private JFrame frame;
+	public void RemoveBlock(){
+		frame = new JFrame();
+		
+		errorMessage = new JLabel();
+		errorMessage.setForeground(Color.RED);
+		errorMessage.setBounds(0, 250, 400, 200);
+		
+		moveBlockLabel = new JLabel();
+		moveBlockLabel.setText("Remove the block from a level's grid");
+		moveBlockLabel.setBounds(80, 0, 450, 50);
 
+		levelNumber = new JLabel();
+		levelNumber.setText("Specify the level: ");
+		levelNumber.setBounds(0, 60,200,30);
 
-            Box.setSize(800,800);
-            Box.setLayout(null);
-            Box.setVisible(true);
-    }
+		levelField = new JTextField();
+		levelField.setBounds(200, 60, 200, 30);
+		
+		oldGridHorizontalValue = new JLabel();
+		oldGridHorizontalValue.setText("Current horizontal position: ");
+		oldGridHorizontalValue.setBounds(0, 115, 200, 30);
+
+		oldGridHorizontalField = new JTextField();
+		oldGridHorizontalField.setBounds(200, 115, 200, 30);
+
+		oldGridVerticalValue = new JLabel();
+		oldGridVerticalValue.setText("Current vertical position: ");
+		oldGridVerticalValue.setBounds(0, 165, 200, 30);
+
+		oldGridVerticalField = new JTextField();
+		oldGridVerticalField.setBounds(200, 165, 200, 30);
+
+		
+		removeButton = new JButton();
+		removeButton.setBounds(135,220,200,50);
+		removeButton.setText("Remove");
+		
+		frame.setSize(550, 350);
+		frame.setLayout(null);
+		frame.setVisible(true);
+		frame.getContentPane().setBackground(Color.PINK);
+
+		frame.add(errorMessage);
+		
+		frame.add(levelField);
+		
+		frame.add(moveBlockLabel);
+		
+		frame.add(levelNumber);
+		frame.add(oldGridHorizontalField);
+		frame.add(oldGridHorizontalValue);
+		frame.add(oldGridVerticalField);
+		frame.add(oldGridVerticalValue);
+        frame.add(removeButton);
+		
+		removeButton.addActionListener(new ActionListener() {
+
+						
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			int level = Integer.parseInt(levelField.getText());
+			int oldGridHorizontalPosition = Integer.parseInt(oldGridHorizontalField.getText());
+			int oldGridVerticalPosition = Integer.parseInt(oldGridVerticalField.getText());
+			
+				try {
+					Block223Controller.removeBlock(level, oldGridHorizontalPosition, oldGridVerticalPosition);
+                    frame.dispose();
+				} catch (Exception er) {
+					er.printStackTrace();
+				}
+	
+			}
+		});
+	}
 }
