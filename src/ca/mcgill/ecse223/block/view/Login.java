be191e2704bf4a3c0401d0a30af04e74813a7b0a
Mairead Maloney <mairead.maloney@mail.mcgill.ca>
2019-03-24 15:21:09 -0400

    ?

--- src/ca/mcgill/ecse223/block/view/Login.java
+++ src/ca/mcgill/ecse223/block/view/Login.java
@@ -1,30 +1,60 @@
 package ca.mcgill.ecse223.block.view;
 
+import ca.mcgill.ecse223.block.controller.*;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Login extends JFrame {
-    public void login(){
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -6987093818977061800L;
+
+	public void login() {
         JFrame Flogin = new JFrame();
         JButton Blogin = new JButton();
-        //TODO Mairead
+        JButton BRegister = new JButton();
+        //JButton PlayAsAdmin = new JButton();
 
-        Blogin.setBounds(100,200,200,50);
+
+        Blogin.setBounds(100, 200, 200, 50);
         Blogin.setText("Log In");
         Blogin.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                GameScreen GS = new GameScreen();
-                //TODO check inputs
-                GS.GameScreen();
+            	SignIn sg = new SignIn();
+            	sg.SignIn();
             }
+            	
         });
+
         Flogin.add(Blogin);
 
-        Flogin.setSize(800,600);
+
+
+        BRegister.setBounds(100, 300, 200, 50);
+        BRegister.setText("Register");
+
+        BRegister.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                SignUp su = new SignUp();
+                su.SignUp();
+            }
+        });
+
+        Flogin.add(BRegister);
+
+
+        Flogin.add(Blogin);
+
+        Flogin.setSize(800, 600);
         Flogin.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         Flogin.setLayout(null);
         Flogin.setVisible(true);
     }
-}
+        
+        
+            
+    	}
